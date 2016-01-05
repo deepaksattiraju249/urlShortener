e46e7c8e43c6a69d96a773d54c66d0c32bb0553e
deepak.sattiraju <deepak.sattiraju@snapdeal.com>
2016-01-05 10:55:10 +0530

    Deleting

--- urlShortener/shortener/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
-class Url(models.Model):
-	shortenedUrl = models.CharField(max_length=100)
-	actualUrl = models.CharField(max_length=200)
