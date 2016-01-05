225a67ab4a992886e8eee9d3e6c604d6be32f47d
deepak.sattiraju <deepak.sattiraju@snapdeal.com>
2016-01-05 15:51:25 +0530

    Added URL Validation

--- urlShortener/shortener/views.py
+++ urlShortener/shortener/views.py
@@ -4,8 +4,10 @@ from models import Url
 from django.template.loader import get_template
 from shortener.base62 import *
 from django.template import Context
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 # Create your views here.
-
+val = URLValidator()
 def shortenUrl(request):
 	if request.method == "GET":
 		t = get_template("index.html")
@@ -14,6 +16,14 @@ def shortenUrl(request):
 		url = request.POST.get("urlToShorten","")
 		if url == "":
 			return HttpResponseRedirect("/")
+		try:
+			if not ("http://" in url) or not ("https://" in url):
+				url="http://"+url
+			val(url)
+		except ValidationError,e:
+			t = get_template("invalid.html")
+			return HttpResponse(t.render())
+
 		url = url.replace("http://","")
 		url = url.replace("https://","")
 		QS = Url.objects.all().filter(actualUrl=url)
@@ -32,6 +42,9 @@ def shortenUrl(request):
 		return HttpResponse(t.render(Context({"actual_url":url, "shortened_url":dehydrate(mUrl.id)})))
 
 def redirect(request):
+	if(request.path_info=="/"):
+		return HttpResponseRedirect("/")
+
 	idInDB = saturate(request.path_info.replace("/",""))
 	QuerySet = Url.objects.all().filter(id=idInDB)
 	if(len(QuerySet)==0):
