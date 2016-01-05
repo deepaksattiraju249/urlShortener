2cd93e58189dc46575e4fb71613a34b2feb15f87
deepak.sattiraju <deepak.sattiraju@snapdeal.com>
2016-01-05 15:11:45 +0530

    UI Added

--- urlShortener/shortener/views.py
+++ urlShortener/shortener/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from models import Url
+from django.template.loader import get_template
 from shortener.base62 import *
+from django.template import Context
 # Create your views here.
 
 def shortenUrl(request):
 	if request.method == "GET":
-		return HttpResponse("GOT")
+		t = get_template("index.html")
+		return HttpResponse(t.render())
 	if request.method == "POST":
 		url = request.POST.get("urlToShorten","")
 		if url == "":
 			return HttpResponseRedirect("/")
 		mUrl = Url()
+		url = url.replace("http://","")
+		url = url.replace("https://","")
 		mUrl.actualUrl = url
 		mUrl.save()
 		# mUrl.shortenedUrl = shorten(url)
+		t = get_template("shortened.html")
 
-		return HttpResponse(dehydrate(mUrl.id))
+		return HttpResponse(t.render(Context({"actual_url":url, "shortened_url":dehydrate(mUrl.id)})))
 
 def redirect(request):
 	idInDB = saturate(request.path_info.replace("/",""))
@@ -24,5 +30,5 @@ def redirect(request):
 	if(len(QuerySet)==0):
 		return HttpResponseRedirect("/")
 	else:
-		return HttpResponseRedirect("http://www.google.com")
+		return HttpResponseRedirect("http://"+QuerySet[0].actualUrl)
 	return HttpResponse(request.path_info)
--- urlShortener/urlShortener/settings.py
+++ urlShortener/urlShortener/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'urlShortener.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,9 +65,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'debug':True,
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'urlShortener.wsgi.application'
 
@@ -120,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
